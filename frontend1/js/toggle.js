0fb5f4e9eb9bfbf628fcf667b28a72a6ee22eb4c
Bharani <24wh1a6623@bvrithyderabad.edu.in>
2026-06-20 11:00:43 +0530

    html5

--- frontend1/js/toggle.js
+++ frontend1/js/toggle.js
@@ -2,20 +2,21 @@ var users=[
     {
         "name" : "john doe",
         "gender" : "male",
-        "image" : "../images/john.png"
+        "image" : "/image/john.png"
     },
     {
         "name" : "jane doe",
         "gender" : "female",
-        "image" : "../images/jane.png"
+        "image" : "/image/jane.png"
     }
 ]
+
 var curId = 0;
 
 function toggle(){
-    curId = (curId + 1)%2
+    curId = (curId + 1)%2;
     var user = users[curId];
-    document.getElementById("user-img").src=user.image;
+    document.getElementById("user-img").src = user.image;
 
     document.getElementById("user-name").innerText=user.name;
     document.getElementById("user-gender").innerText=user.gender;

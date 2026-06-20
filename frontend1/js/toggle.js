f277b0b8041667337b01b6be66ba3acfe71a4f70
Bharani <24wh1a6623@bvrithyderabad.edu.in>
2026-06-20 10:03:31 +0530

    html4

---
+++ frontend1/js/toggle.js
@@ -0,0 +1,23 @@
+var users=[
+    {
+        "name" : "john doe",
+        "gender" : "male",
+        "image" : "../images/john.png"
+    },
+    {
+        "name" : "jane doe",
+        "gender" : "female",
+        "image" : "../images/jane.png"
+    }
+]
+var curId = 0;
+
+function toggle(){
+    curId = (curId + 1)%2
+    var user = users[curId];
+    document.getElementById("user-img").src=user.image;
+
+    document.getElementById("user-name").innerText=user.name;
+    document.getElementById("user-gender").innerText=user.gender;
+        
+}
